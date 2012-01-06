b3a5fe8be38a97600e904c703993a4f4beefe2c0
Roman Birg <romanbirg@gmail.com>
2012-01-06 12:22:21 -0600

    Add a 2G toggle for completeness sake =D

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -59,6 +59,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
     private static final int CMD_TOGGLE_LTE = 7; // not used yet
+    private static final int CMD_TOGGLE_2G = 8; // not used yet
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -290,6 +291,19 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
         mPhone.setPreferredNetworkType(network,
                 mMainThreadHandler.obtainMessage(CMD_TOGGLE_LTE));
+        Settings.Secure.putInt(mApp.getContentResolver(),
+                Settings.Secure.PREFERRED_NETWORK_MODE, network);
+    }
+    
+    public void toggle2G(boolean on) {
+        int network = -1;
+        if (on) {
+            network = Phone.NT_MODE_GSM_ONLY;
+        } else {
+            network = Phone.NT_MODE_WCDMA_PREF;
+        }
+        mPhone.setPreferredNetworkType(network,
+                mMainThreadHandler.obtainMessage(CMD_TOGGLE_2G));
         Settings.Secure.putInt(mApp.getContentResolver(),
                 Settings.Secure.PREFERRED_NETWORK_MODE, network);
     }
