8c1defc5e82b8dbe8ff9837d02d3bb6c85adcd61
Mike Wilson <mwilson@acesappraisal.com>
2012-01-27 20:18:39 -0500

    Landscape while in call.
    Change-Id: I503fa8b3da9d02439385d52527b3da7a8d5658bb
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -194,6 +194,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String SIP_SETTINGS_CATEGORY_KEY =
             "sip_settings_category_key";
     private static final String BUTTON_EXIT_TO_HOMESCREEN_KEY = "button_exit_to_home_screen_key";
+    private static final String BUTTON_LANDSCAPE_KEY = "button_landscape_key";
 
     private Intent mContactListIntent;
 
@@ -294,7 +295,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private SipSharedPreferences mSipSharedPreferences;
 
     private CheckBoxPreference mButtonExitToHomeScreen;
-
+	private CheckBoxPreference mButtonLandscape;
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
             this.name = name;
@@ -307,7 +308,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     /**
      * Forwarding settings we are going to save.
      */
-    private static final int [] FORWARDING_SETTINGS_REASONS = new int[] {
+    static final int [] FORWARDING_SETTINGS_REASONS = new int[] {
         CommandsInterface.CF_REASON_UNCONDITIONAL,
         CommandsInterface.CF_REASON_BUSY,
         CommandsInterface.CF_REASON_NO_REPLY,
