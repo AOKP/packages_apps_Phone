649606e83fb3b6482d32bb2f9c64fb1c98be3bad
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-02-18 22:02:22 -0200

    Port network mode toggle from CM10
    	commit 43575664b23d2ea1766210cea99f184e1732860b
    	Author: Ricardo Cerqueira <github@cerqueira.org>
    	Date:   Fri Apr 27 17:39:37 2012 +0100
    
    	    PhoneToggler: Fix toggler on LTE+GSM phones
    
    	    NT_MODE_LTE_GSM_WCDMA was missing from the lists of acceptable
    	    network modes
    
    	    Change-Id: I6c3c38d63902c1c59cfa2e61ed5d8bcdd5bcb5f0
    
    	commit d1dc25f1b37fca42ce2834fe3bb3217800422f7e
    	Author: Emerson Pinter <dev@pinter.com.br>
    	Date:   Sat Feb 18 22:02:22 2012 -0200
    
    	Makes network mode toggle work. Port from CM9.
    
    	Added LTE_ONLY needed code to PhoneToggler.
    	Added LTE to the double check MODE_GLOBAL condition on Settings and
    	PhoneToggler.
    
    	Integrated patch by Danny Baumann to make PhoneToggler accept the same
    	values as are configurable in settings, from change:
    	http://review.cyanogenmod.com/#change,14120
    
    	Tested-by: Emerson Pinter
    	Signed-off-by: Emerson Pinter <dev@pinter.com.br>
    
    	*** Settings.java and PhoneToggler.java original authors:
    
    	commit 3e6b2ca4c969aedeefa9a10bb85e9c52bc43a574
    	Author: Emilio López <turl@tuxfamily.org>
    	Date:   Wed Sep 14 23:50:06 2011 +0000
    
    	    Phone: Double check mode before changing, part 2
    
    	    A modem might report world phone mode when it really is only GSM or
    	    CDMA capable, making the system switch to an invalid mode which
    	    will later break the telephony code.
    	    This simple check enforces the world phone overlay which is already
    	    in place and will only consider it a correct value in that case.
    
    	    This complements a commit with the same title by applying the same
    	    logic to another code path that uses similar checks.
    
    	    Change-Id: Ib7332511a693d40f53a79ba98844c9117871228a
    
    	commit 19215c8f614e30211b2aca1512bb8444da55bdda
    	Author: Emilio López <turl@tuxfamily.org>
    	Date:   Wed Sep 14 23:50:06 2011 +0000
    
    	    Phone: Double check mode before changing
    
    	    A modem might report world phone mode when it really is only GSM or
    	    CDMA capable, making the system switch to an invalid mode which
    	    will later break the telephony code.
    	    This simple check enforces the world phone overlay which is already
    	    in place and will only consider it a correct value in that case.
    
    	    Change-Id: Iedbc529497b2779f3a12ff56ca1b028cc9434227
    
    	commit 0d396f79514173e06c5334a40f23090933e0a806
    	Author: piaf <piaf@mediacenter-desktop.(none)>
    	Date:   Tue Aug 3 00:56:09 2010 +0100
    
    	    Added permission control for the 2G/3G switch
    
    	commit 5c85d5417a67b02be895a8878c7fa86bc992cd8e
    	Author: Mediacenter <mediacenter@mediacenter-desktop.(none)>
    	Date:   Sat Jul 31 01:45:03 2010 +0100
    
    	    Added support for Changing Phone network mode
    	    Todo: Add permission control
    
    	Change-Id: I84db8082b4e35262f1c6cdb8d1156f3cbf997a99
    
    Change-Id: I4b68ec178eafdd6a50fa111dd71cc5fa38f3968c

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -181,6 +181,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                     (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 
             cm.setMobileDataEnabled(mButtonDataEnabled.isChecked());
+
+            Intent intent = new Intent(PhoneToggler.MOBILE_DATA_CHANGED);
+            intent.putExtra(PhoneToggler.NETWORK_MODE, mButtonDataEnabled.isChecked());
+            mPhone.getContext().sendBroadcast(intent);
             return true;
         } else if (preference == mLteDataServicePref) {
             String tmpl = android.provider.Settings.Global.getString(getContentResolver(),
@@ -415,6 +419,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                 //Set the modem network mode
                 mPhone.setPreferredNetworkType(modemNetworkMode, mHandler
                         .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
+
+                Intent intent = new Intent(PhoneToggler.NETWORK_MODE_CHANGED);
+                intent.putExtra(PhoneToggler.NETWORK_MODE, buttonNetworkMode);
+                mPhone.getContext().sendBroadcast(intent, PhoneToggler.CHANGE_NETWORK_MODE_PERM);
             }
         }
 
@@ -461,6 +469,7 @@ public class MobileNetworkSettings extends PreferenceActivity
                             settingsNetworkMode);
                 }
 
+                boolean isLteOnCdma = mPhone.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE;
                 //check that modemNetworkMode is from an accepted value
                 if (modemNetworkMode == Phone.NT_MODE_WCDMA_PREF ||
                         modemNetworkMode == Phone.NT_MODE_GSM_ONLY ||
@@ -469,7 +478,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                         modemNetworkMode == Phone.NT_MODE_CDMA ||
                         modemNetworkMode == Phone.NT_MODE_CDMA_NO_EVDO ||
                         modemNetworkMode == Phone.NT_MODE_EVDO_NO_CDMA ||
-                        modemNetworkMode == Phone.NT_MODE_GLOBAL ||
+                        //A modem might report world phone sometimes
+                        //but it's not true. Double check here
+                        ((getResources().getBoolean(R.bool.world_phone) == true || isLteOnCdma) &&
+                            modemNetworkMode == Phone.NT_MODE_GLOBAL) ||
                         modemNetworkMode == Phone.NT_MODE_LTE_CDMA_AND_EVDO ||
                         modemNetworkMode == Phone.NT_MODE_LTE_GSM_WCDMA ||
                         modemNetworkMode == Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA ||
@@ -502,6 +514,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                     UpdatePreferredNetworkModeSummary(modemNetworkMode);
                     // changes the mButtonPreferredNetworkMode accordingly to modemNetworkMode
                     mButtonPreferredNetworkMode.setValue(Integer.toString(modemNetworkMode));
+
+                    Intent intent = new Intent(PhoneToggler.NETWORK_MODE_CHANGED);
+                    intent.putExtra(PhoneToggler.NETWORK_MODE, modemNetworkMode);
+                    mPhone.getContext().sendBroadcast(intent, PhoneToggler.CHANGE_NETWORK_MODE_PERM);
                 } else if (modemNetworkMode == Phone.NT_MODE_LTE_ONLY) {
                     // LTE Only mode not yet supported on UI, but could be used for testing
                     if (DBG) log("handleGetPreferredNetworkTypeResponse: lte only: no action");
@@ -521,6 +537,10 @@ public class MobileNetworkSettings extends PreferenceActivity
                 android.provider.Settings.Global.putInt(mPhone.getContext().getContentResolver(),
                         android.provider.Settings.Global.PREFERRED_NETWORK_MODE,
                         networkMode );
+
+                Intent intent = new Intent(PhoneToggler.NETWORK_MODE_CHANGED);
+                intent.putExtra(PhoneToggler.NETWORK_MODE, networkMode);
+                mPhone.getContext().sendBroadcast(intent, PhoneToggler.CHANGE_NETWORK_MODE_PERM);
             } else {
                 mPhone.getPreferredNetworkType(obtainMessage(MESSAGE_GET_PREFERRED_NETWORK_TYPE));
             }
