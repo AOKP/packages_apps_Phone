73567aed6619d71ff74177114e1d56de6597458d
Matanya Elchanani <melchanani@hotmail.com>
2012-04-23 22:25:12 -0400

    Add Ring Delay feature
    This allows the user to set the number of seconds
    before the ringer goes off. This is useful when
    vibrate is enabled to get "vibrate before ring".
    
    Change-Id: If161e246e7ed3bfbdd55dca098aff5d7a13ab215

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -150,6 +150,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_DTMF_KEY   = "button_dtmf_settings";
     private static final String BUTTON_RETRY_KEY  = "button_auto_retry_key";
     private static final String BUTTON_TTY_KEY    = "button_tty_mode_key";
+    /* package */ static final String BUTTON_RING_DELAY_KEY = "button_ring_delay_key";
     private static final String BUTTON_HAC_KEY    = "button_hac_key";
     
     private static final String BUTTON_LANDSCAPE_KEY = "button_landscape_key";
@@ -231,6 +232,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
+    private ListPreference mButtonRingDelay;
     private ListPreference mButtonSipCallOptions;
     private ListPreference mVoicemailProviders;
     private PreferenceScreen mVoicemailSettings;
@@ -444,6 +446,8 @@ public class CallFeaturesSetting extends PreferenceActivity
             return true;
         } else if (preference == mButtonTTY) {
             return true;
+        } else if (preference == mButtonRingDelay) {
+            return true;
         } else if (preference == mButtonAutoRetry) {
             android.provider.Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     android.provider.Settings.System.CALL_AUTO_RETRY,
@@ -518,6 +522,9 @@ public class CallFeaturesSetting extends PreferenceActivity
             mVoicemailNotificationVibrateWhen.setValue((String) objValue);
             mVoicemailNotificationVibrateWhen.setSummary(
                     mVoicemailNotificationVibrateWhen.getEntry());
+        } else if (preference == mButtonRingDelay) {
+            mButtonRingDelay.setValue((String) objValue);
+            mButtonRingDelay.setSummary(mButtonRingDelay.getEntry());
         } else if (preference == mButtonSipCallOptions) {
             handleSipCallOptionsChange(objValue);
         }
@@ -1410,6 +1417,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         
         mButtonHAC = (CheckBoxPreference) findPreference(BUTTON_HAC_KEY);
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
+        mButtonRingDelay = (ListPreference) findPreference(BUTTON_RING_DELAY_KEY);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
         if (mVoicemailProviders != null) {
             mVoicemailProviders.setOnPreferenceChangeListener(this);
@@ -1455,6 +1463,15 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mButtonTTY);
                 mButtonTTY = null;
+            }
+        }
+
+        if (mButtonRingDelay != null) {
+            if (getResources().getBoolean(R.bool.ringdelay_enabled)) {
+                mButtonRingDelay.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mButtonRingDelay);
+                mButtonRingDelay = null;
             }
         }
 
@@ -1584,6 +1601,13 @@ public class CallFeaturesSetting extends PreferenceActivity
                     Phone.TTY_MODE_OFF);
             mButtonTTY.setValue(Integer.toString(settingsTtyMode));
             updatePreferredTtyModeSummary(settingsTtyMode);
+        }
+
+        if (mButtonRingDelay != null) {
+            CharSequence mPrefEntry = mButtonRingDelay.getEntry();
+            if (mPrefEntry != null) {
+                mButtonRingDelay.setSummary(mPrefEntry);
+            }
         }
     }
 
--- src/com/android/phone/Ringer.java
+++ src/com/android/phone/Ringer.java
@@ -17,6 +17,7 @@
 package com.android.phone;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -30,6 +31,7 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -172,8 +174,10 @@ public class Ringer {
 
             makeLooper();
             if (mFirstRingEventTime < 0) {
-                mFirstRingEventTime = SystemClock.elapsedRealtime();
-                mRingHandler.sendEmptyMessage(PLAY_RING_ONCE);
+                SharedPreferences callSettings = PreferenceManager.getDefaultSharedPreferences(mContext);
+                int ringDelay = Integer.valueOf(callSettings.getString(CallFeaturesSetting.BUTTON_RING_DELAY_KEY, "0"));
+                mFirstRingEventTime = SystemClock.elapsedRealtime() + ringDelay*1000;
+                mRingHandler.sendEmptyMessageDelayed(PLAY_RING_ONCE, ringDelay*1000);
             } else {
                 // For repeat rings, figure out by how much to delay
                 // the ring so that it happens the correct amount of
@@ -190,7 +194,9 @@ public class Ringer {
                     // We've gotten two ring events so far, but the ring
                     // still hasn't started. Reset the event time to the
                     // time of this event to maintain correct spacing.
-                    mFirstRingEventTime = SystemClock.elapsedRealtime();
+                    if (mFirstRingEventTime <= SystemClock.elapsedRealtime()) {
+                        mFirstRingEventTime = SystemClock.elapsedRealtime();
+                    }
                 }
             }
         }
