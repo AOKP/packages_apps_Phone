552fb9a571cb4612ad3e53d6254d77e2065501d1
Francesco <francescoforesta@email.it>
2013-04-13 14:04:29 +0200

    Phone blacklist: block private numbers functionality
    PS2 : fix rebase derp ( http://review.cyanogenmod.org/#/c/35850 )
    
    Change-Id: Ib271a79889e6e5ade6107c024301b045596ee151
    Signed-off-by: ChampionSwimmer <android@championswimmer.tk>

--- src/com/android/phone/Blacklist.java
+++ src/com/android/phone/Blacklist.java
@@ -26,10 +26,13 @@ class Blacklist {
     private Context mContext;
     private HashSet<PhoneNumber> mList = new HashSet<PhoneNumber>();
 
+    public final static String PRIVATE_NUMBER ="0000";
+
     // Blacklist matching type
     public final static int MATCH_NONE = 0;
-    public final static int MATCH_LIST = 1;
-    public final static int MATCH_REGEX = 2;
+    public final static int MATCH_PRIVATE = 1;
+    public final static int MATCH_LIST = 2;
+    public final static int MATCH_REGEX = 3;
 
     public Blacklist(Context context) {
         mContext = context;
@@ -120,7 +123,7 @@ class Blacklist {
     /**
      * Check if the number is in the blacklist
      * @param s: Number to check
-     * @return one of: MATCH_NONE, MATCH_LIST or MATCH_REGEX
+     * @return one of: MATCH_NONE, MATCH_PRIVATE, MATCH_LIST or MATCH_REGEX
      */
     public int isListed(String s) {
         if (!PhoneUtils.PhoneSettings.isBlacklistEnabled(mContext)) {
@@ -132,9 +135,15 @@ class Blacklist {
     /**
      * See if the number is in the blacklist
      * @param s: Number to check
-     * @return one of: MATCH_NONE, MATCH_LIST or MATCH_REGEX
+     * @return one of: MATCH_NONE, MATCH_PRIVATE, MATCH_LIST or MATCH_REGEX
      */
     private int matchesBlacklist(String s) {
+        // Private number matching
+        if (PhoneUtils.PhoneSettings.isBlacklistPrivateNumberEnabled(mContext)
+                && s.equals(PRIVATE_NUMBER)) {
+            return MATCH_PRIVATE;
+        }
+
         // Standard list matching
         if (mList.contains(new PhoneNumber(s))) {
             return MATCH_LIST;
