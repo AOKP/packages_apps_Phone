LineageOS/android_packages_apps_Phone

Phone: Add Blacklist functionality
Forward port the Blacklist functionality from CM7.

Original code written by Cytown, modified by Cyanogen (2011-01-04),
nhnt11 (2011-05-24) and maniac103 (2012-10-02).

Change-Id: Ib6b8ed4b3410d8553ca061086ba7bcf3b059b1d0

src/com/android/phone/PhoneUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Blacklist - Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +25,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -391,33 +393,38 @@ public class PhoneUtils {
 
     static class PhoneSettings {
         static boolean vibOn45Secs(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_45", false);
+            return getPrefs(context).getBoolean("button_vibrate_45", false);
         }
         static boolean vibHangup(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_hangup", false);
+            return getPrefs(context).getBoolean("button_vibrate_hangup", false);
         }
         static boolean vibOutgoing(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_outgoing", false);
+            return getPrefs(context).getBoolean("button_vibrate_outgoing", false);
         }
         static boolean vibCallWaiting(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_call_waiting", false);
+            return getPrefs(context).getBoolean("button_vibrate_call_waiting", false);
         }
         static boolean showInCallEvents(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_show_ssn_key", false);
+            return getPrefs(context).getBoolean("button_show_ssn_key", false);
         }
         static boolean showCallLogAfterCall(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_calllog_after_call", false);
+            return getPrefs(context).getBoolean("button_calllog_after_call", false);
         }
         static int flipAction(Context context) {
-            String s = PreferenceManager.getDefaultSharedPreferences(context)
-                      .getString("flip_action", "0");
+            String s = getPrefs(context).getString("flip_action", "0");
             return Integer.parseInt(s);
+        }
+        static boolean isBlacklistEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_enable_blacklist", false);
+        }
+        static boolean isBlacklistNotifyEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_nofify", false);
+        }
+        static boolean isBlacklistRegexEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_blacklist_regex", false);
+        }
+        private static SharedPreferences getPrefs(Context context) {
+            return PreferenceManager.getDefaultSharedPreferences(context);
         }
     }
 
@@ -1634,7 +1641,11 @@ public class PhoneUtils {
             // return it to the user.
 
             cit = new CallerInfoToken();
-            cit.currentInfo = (CallerInfo) userDataObject;
+            if (userDataObject instanceof String) { // only blacklist will cause this, so just ignore this.
+                cit.currentInfo = new CallerInfo();
+            } else {
+                cit.currentInfo = (CallerInfo) userDataObject;
+            }
             cit.asyncQuery = null;
             cit.isFinal = true;
             // since the query is already done, call the listener.

AOKP/packages_apps_Phone

Original code written for CM7 by Cytown, Cyanogen and nhnt11.
Modified for CM10.1 by Danny Baumann and dvtonder.
Drawables by Björn Lundén
cherry-picked from
http://review.cyanogenmod.org/#/c/34904/20
and fixed conflicts to work on AOKP.
PS2:rebase
PS3:use getPrefs(context) for CM Advanced settings
Change-Id: Ifac0a758d0f0bab166acf1e5a569327a15e197be
Signed-off-by: ChampionSwimmer <android@championswimmer.tk>

src/com/android/phone/PhoneUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Blacklist - Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +25,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -391,20 +393,16 @@ public class PhoneUtils {
 
     static class PhoneSettings {
         static boolean vibOn60Secs(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_60", false);
+            return getPrefs(context).getBoolean("button_vibrate_60", false);
         }
         static boolean vibHangup(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_hangup", false);
+            return getPrefs(context).getBoolean("button_vibrate_hangup", false);
         }
         static boolean vibOutgoing(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_outgoing", false);
+            return getPrefs(context).getBoolean("button_vibrate_outgoing", false);
         }
         static boolean vibCallWaiting(Context context) {
-            return PreferenceManager.getDefaultSharedPreferences(context)
-                      .getBoolean("button_vibrate_call_waiting", false);
+            return getPrefs(context).getBoolean("button_vibrate_call_waiting", false);
         }
 
         /* Voice quality filter */
@@ -434,6 +432,18 @@ public class PhoneUtils {
             String conf = PreferenceManager.getDefaultSharedPreferences(context)
                     .getString("button_voice_quality_key", "0");
             return Integer.parseInt(conf);
+        }
+        static boolean isBlacklistEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_enable_blacklist", false);
+        }
+        static boolean isBlacklistNotifyEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_nofify", false);
+        }
+        static boolean isBlacklistRegexEnabled(Context context) {
+            return getPrefs(context).getBoolean("button_blacklist_regex", false);
+        }
+        private static SharedPreferences getPrefs(Context context) {
+            return PreferenceManager.getDefaultSharedPreferences(context);
         }
     }
 
@@ -1650,7 +1660,11 @@ public class PhoneUtils {
             // return it to the user.
 
             cit = new CallerInfoToken();
-            cit.currentInfo = (CallerInfo) userDataObject;
+            if (userDataObject instanceof String) { // only blacklist will cause this, so just ignore this.
+                cit.currentInfo = new CallerInfo();
+            } else {
+                cit.currentInfo = (CallerInfo) userDataObject;
+            }
             cit.asyncQuery = null;
             cit.isFinal = true;
             // since the query is already done, call the listener.