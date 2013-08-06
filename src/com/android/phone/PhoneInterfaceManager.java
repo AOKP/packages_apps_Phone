b250cb3689200e66667437df9e1ff5a702771f60
Hariprasad Jayakumar <hjayak@codeaurora.org>
2013-08-05 17:10:39 -0700

    Phone: Check for radio state properly
    The implementation of ITelephony.isRadioOn() method checks for
    service state instead of radio state. In cases of basebandless
    targets, service state is not changed and would remain in the
    initialized OUT_OF_SERVICE state. This causes delay in shutdown
    procedure for such targets.
    
    Hence a method isRadioOn() is added in phone interface and is
    used to get the radio state.
    
    Change-Id: I5a60aa39170f9d2863e95109dfd54cbd29ffdf47
    CRs-Fixed: 502317

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -564,7 +564,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
 
     public boolean isRadioOn() {
-        return mPhone.getServiceState().getVoiceRegState() != ServiceState.STATE_POWER_OFF;
+        return mPhone.isRadioOn();
     }
 
     public void toggleRadioOnOff() {
@@ -573,7 +573,7 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
     public boolean setRadio(boolean turnOn) {
         enforceModifyPermission();
-        if ((mPhone.getServiceState().getVoiceRegState() != ServiceState.STATE_POWER_OFF) != turnOn) {
+        if (mPhone.isRadioOn() != turnOn) {
             toggleRadioOnOff();
         }
         return true;
