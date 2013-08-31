a8fb0b898d26e94a1f88b9269a6d1caedb3fc3b0
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-08-31 22:47:01 +0200

    Phone: Voicemail...be gone!
    Change-Id: Ib8905249cc529a93b9114c3acca0f5150cb4b245
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -1437,7 +1437,6 @@ public class NotificationMgr implements CallerInfoAsyncQuery.OnQueryCompleteList
             if (vibrate) {
                 notification.defaults |= Notification.DEFAULT_VIBRATE;
             }
-            notification.flags |= Notification.FLAG_NO_CLEAR;
             configureLedNotification(notification);
             mNotificationManager.notify(VOICEMAIL_NOTIFICATION, notification);
         } else {
