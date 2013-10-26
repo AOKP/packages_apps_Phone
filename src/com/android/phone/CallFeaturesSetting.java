9cb11beeab8002d98150c82e25a5a3269e396695
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-10-26 23:42:47 +0200

    Show checkbox for message wait indicator for everyone
    This will show the settings checkbox for all devices regardles of
    the sprint_mwi_quirk overlay settings.
    
    The current logic was to hide the checkbox if the overlay was
    missing or set to false. Additionally the default value was false,
    which will never show an indicator.
    
    Change-Id: I627071f4d83d3416c905161f1876cffd97dd50f1
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1600,15 +1600,8 @@ public class CallFeaturesSetting extends PreferenceActivity
         mPlayDtmfTone = (CheckBoxPreference) findPreference(BUTTON_PLAY_DTMF_TONE);
         mMwiNotification = (CheckBoxPreference) findPreference(BUTTON_MWI_NOTIFICATION_KEY);
         if (mMwiNotification != null) {
-            if (getResources().getBoolean(R.bool.sprint_mwi_quirk)) {
-                mMwiNotification.setOnPreferenceChangeListener(this);
-            } else {
-                PreferenceScreen voicemailScreen = (PreferenceScreen) findPreference(BUTTON_VOICEMAIL_CATEGORY_KEY);
-                voicemailScreen.removePreference(mMwiNotification);
-                mMwiNotification = null;
-            }
+            mMwiNotification.setOnPreferenceChangeListener(this);
         }
-
         mDialpadAutocomplete = (CheckBoxPreference) findPreference(BUTTON_DIALPAD_AUTOCOMPLETE);
         mButtonDTMF = (ListPreference) findPreference(BUTTON_DTMF_KEY);
         mButtonAutoRetry = (CheckBoxPreference) findPreference(BUTTON_RETRY_KEY);
